1265df056c9b4b1b9d7720bb7d388f733d53b685
prince <princeakpabio90@gmail.com>
2020-12-08 00:34:15 +0100

    added responsive nav bar

--- src/components/navigation/naviagtion.jsx
+++ src/components/navigation/naviagtion.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { MenuItems } from './menuItems';
 // import Scroll from 'react-scroll';
 import { Link} from 'react-scroll';
@@ -6,28 +6,35 @@ import { Link} from 'react-scroll';
 // const ScrollLink = Scroll.Link;
 
 const Naviagtion = () => {
+ const [menu, setMenu] = useState(false)
+
+ const ToggleMenu = () => {
+  setMenu(prevMenu => !prevMenu)
+ };
  return (
   <div className='Nav'>
    <span id='Nav-logo'>Akpabio Prince</span>
 
-    <div className="Nav-menu-wrapper">
+    <div className={`Nav-menu-wrapper ${menu && 'active'}`}>
      <ul className='Nav-menu'>
      {
-      MenuItems.map((menu, idx) => (
-     <li className="Nav-menu-items" key={idx}>
+      MenuItems.map((Menu, idx) => (
+     <li className={`Nav-menu-items ${menu && 'active'}`} key={idx}>
         <Link
-         to={menu.link}
+         to={Menu.link}
          activeClass="active"
          smooth={true}
          duration={500}
-        >{menu.name}</Link>
+        >{Menu.name}</Link>
      </li>
       ))
      }
      </ul>
     </div>
 
-    <span className="Nav-mb-menu"> <ion-icon name="menu-outline"></ion-icon> </span>
+   <span className="Nav-mb-menu"
+   onClick={ToggleMenu}
+   > {menu ? <ion-icon name="close-outline"></ion-icon> :<ion-icon name="menu-outline"></ion-icon>} </span>
    
   </div>
  )

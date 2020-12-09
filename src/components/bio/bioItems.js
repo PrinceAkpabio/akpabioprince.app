5c98e49f5a642dc4a4b26257468c25f29c741898
prince <princeakpabio90@gmail.com>
2020-12-09 03:10:42 +0100

    added bio component with animation and created custom hook for it

---
+++ src/components/bio/bioItems.js
@@ -0,0 +1,27 @@
+import bio1 from '../../Assets/images/DSC_0326.JPG'
+import bio2 from '../../Assets/images/DSC_1318.JPG'
+import bio3 from '../../Assets/images/DSC_0327.JPG'
+import bio4 from '../../Assets/images/DSC_1325.JPG'
+import bio5 from '../../Assets/images/DSC_1330.JPG'
+import bio6 from '../../Assets/images/DSC_1345.JPG'
+
+export const BioItems = [
+ {
+  img: bio1
+ },
+ {
+  img: bio2
+ },
+ {
+  img: bio3
+ },
+ {
+  img: bio4
+ },
+ {
+  img: bio5
+ },
+ {
+  img: bio6
+ }
+]

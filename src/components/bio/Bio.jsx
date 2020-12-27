b1d29effac4281a0d592081d605e6d67f7ec7d7a
prince <princeakpabio90@gmail.com>
2020-12-27 12:30:25 +0100

    removed warnings for netlify  mergability

--- src/components/bio/Bio.jsx
+++ src/components/bio/Bio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import { BioContent } from './bioItems';
 import waves from '../../Assets/images/waves.svg';
 import { AnimateOnScroll } from '../customHooks/scrollAnimate';
